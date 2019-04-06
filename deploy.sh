c00115cc36cc4ab192d5390f1c786c2461c49fa8
Levertion <26185209+Levertion@users.noreply.github.com>
2019-04-06 21:00:52 +0100

    fix(ci): Add deploy script to work around detached head

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+if [[ `git log -1 --pretty=%B` != "Publish"* ]]; then
+ if [ -z "$CI" ]; then
+    echo "This commit would run the deploy step"
+  else
+    git checkout $TRAVIS_BRANCH
+    if [[ $TRAVIS_COMMIT == `git rev-parse HEAD`]] then
+      git reset --hard $TRAVIS_COMMIT
+      npx lerna publish --dist-tag next --yes
+      git config user.email "travis@travis-ci.org"
+      git config user.name "Travis CI"
+      # Silence it to not expose the GH_TOKEN
+      git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG} > /dev/null 2>&1
+      git push --quiet
+    else
+      echo "Not on latest $TRAVIS_BRANCH, not pushing"
+    fi
+  fi
+fi
